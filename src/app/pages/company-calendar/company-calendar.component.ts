04d55e4e386f481054caf67683870728372ddd46
marti <martinavalkova1999@gmail.com>
2022-07-06 16:58:57 +0300

    Last data fixes

--- src/app/pages/company-calendar/company-calendar.component.ts
+++ src/app/pages/company-calendar/company-calendar.component.ts
@@ -1,22 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {BackendService} from "../../services/backend.service";
 import {Holiday} from "../../interfaces/user";
 import {FormControl, Validators} from "@angular/forms";
 import {SimpleDialogComponent} from "../../components/simple-dialog/simple-dialog.component";
 import {MatDialog} from "@angular/material/dialog";
+import {MatCalendarCellClassFunction} from "@angular/material/datepicker";
 
 @Component({
   selector: 'app-company-calendar',
   templateUrl: './company-calendar.component.html',
-  styleUrls: ['./company-calendar.component.css']
+  styleUrls: ['./company-calendar.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CompanyCalendarComponent implements OnInit {
   //todo: check  Highlighting specific dates in calendar https://material.angular.io/components/datepicker/overview#datepicker-date-class
   selected: Date | null;
   holidays: Holiday[] = []
+  returnStatement: string = ''
   displayedColumns: string[] = ['name', 'date', 'action'];
 
-  holidayNameFC = new FormControl("Ден на Бащата", [Validators.required])
+  holidayNameFC = new FormControl("", [Validators.required])
 
   constructor(private backendService: BackendService, public dialog: MatDialog) {
     this.selected = null;
@@ -31,6 +34,20 @@ export class CompanyCalendarComponent implements OnInit {
   isAdmin() {
     return this.backendService.isAdmin()
   }
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    if (view === 'month') {
+      this.returnStatement = '';
+      this.holidays.forEach((value: Holiday) => {
+        const holidayDate = new Date(value.holiday)
+        if (holidayDate.getFullYear() == cellDate.getFullYear() && holidayDate.getMonth() == cellDate.getMonth() && holidayDate.getDate() == cellDate.getDate()) {
+          this.returnStatement = 'example-custom-date-class';
+        }
+      });
+        return this.returnStatement;
+      }
+    return this.returnStatement;
+  };
 
   createHoliday(holidayDate: Date | null) {
     if (this.holidayNameFC.valid && holidayDate != null) {
